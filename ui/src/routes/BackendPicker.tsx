b54bd470de31e2d7edf05bc154394c841670148e
austin5456 <austin.berry@docker.com>
2024-04-03 15:50:54 -0400

    fix svgs

--- ui/src/routes/BackendPicker.tsx
+++ ui/src/routes/BackendPicker.tsx
@@ -1,4 +1,11 @@
-import { Card, CardActionArea, CardContent, CardMedia, Stack, Typography } from '@mui/material';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export function BackendPicker() {
@@ -6,66 +13,71 @@ export function BackendPicker() {
 
   const options = [
     {
-      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original.svg",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original.svg",
       alt: "go",
       title: "Go",
       description: "Build simple, secure, scalable systems with Go",
-      id: "go"
+      id: "go",
     },
     {
-      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original.svg",
       alt: "nestjs",
       title: "NestJS",
-      description: "A progressive Node.js framework for building efficient, reliable and scalable server-side applications.",
-      id: "nestjs"
+      description:
+        "A progressive Node.js framework for building efficient, reliable and scalable server-side applications.",
+      id: "nestjs",
     },
     {
-      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg",
       alt: "fast api",
       title: "Fast API",
       description: "Create a web API. This will generate a backend server.",
-      id: "fastapi"
+      id: "fastapi",
     },
     {
-      image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg",
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg",
       alt: "asp net",
       title: "ASP.NET",
-      description: "ASP.NET is an open source web framework, created by Microsoft, for building modern web apps and services with .NET.",
-      id: "asp-net"
+      description:
+        "ASP.NET is an open source web framework, created by Microsoft, for building modern web apps and services with .NET.",
+      id: "asp-net",
     },
-  ]
+  ];
 
   return (
     <>
       <Typography variant="h3">Develop with Docker</Typography>
-      <Typography sx={{ mt: 8 }}>What language or framework would you like to use for the backend?</Typography>
+      <Typography sx={{ mt: 8 }}>
+        What language or framework would you like to use for the backend?
+      </Typography>
       <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 2 }}>
-        {
-          options.map((option) => {
-            return (
-              <Card sx={{ height: 300, width: 250 }} key={option.id}>
-                <CardActionArea onClick={() =>
-                  navigate(`/frontend-picker`)
-                }>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={option.image}
-                    alt={option.alt}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {option.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {option.description}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            )
-          })
-        }
+        {options.map((option) => {
+          return (
+            <Card sx={{ height: 300, width: 250 }} key={option.id}>
+              <CardActionArea onClick={() => navigate(`/frontend-picker`)}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={option.image}
+                  alt={option.alt}
+                  sx={{ objectFit: "contain", p: 1 }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {option.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {option.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
       </Stack>
     </>
   );
--- ui/src/routes/FrontendPicker.tsx
+++ ui/src/routes/FrontendPicker.tsx
@@ -1,60 +1,74 @@
-import { Card, CardActionArea, CardContent, CardMedia, Stack, Typography } from '@mui/material';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export function FrontendPicker() {
   const navigate = useNavigate();
 
-  const options = [{
-    image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-    alt: "react",
-    title: "React",
-    description: "The library for web and native user interfaces",
-    id: "react"
-  }, {
-    image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
-    alt: "angular",
-    title: "Angular",
-    description: "The web development framework for building the future",
-    id: "angular"
-  }, {
-    image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg",
-    alt: "vue.js",
-    title: "Vue.js",
-    description: "An approachable, performant and versatile framework for building web user interfaces.",
-    id: "vue-js"
-  }]
+  const options = [
+    {
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+      alt: "react",
+      title: "React",
+      description: "The library for web and native user interfaces",
+      id: "react",
+    },
+    {
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg",
+      alt: "angular",
+      title: "Angular",
+      description: "The web development framework for building the future",
+      id: "angular",
+    },
+    {
+      image:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg",
+      alt: "vue.js",
+      title: "Vue.js",
+      description:
+        "An approachable, performant and versatile framework for building web user interfaces.",
+      id: "vue-js",
+    },
+  ];
 
   return (
     <>
       <Typography variant="h3">Develop with Docker</Typography>
-      <Typography sx={{ mt: 8 }}>What language or framework would you like to use for the frontend?</Typography>
+      <Typography sx={{ mt: 8 }}>
+        What language or framework would you like to use for the frontend?
+      </Typography>
       <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 2 }}>
-        {
-          options.map((option) => {
-            return (
-              <Card sx={{ height: 300, width: 250 }} key={option.id}>
-                <CardActionArea onClick={() =>
-                  navigate(`/project-details`)
-                }>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image={option.image}
-                    alt={option.alt}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {option.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {option.description}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            )
-          })
-        }
+        {options.map((option) => {
+          return (
+            <Card sx={{ height: 300, width: 250 }} key={option.id}>
+              <CardActionArea onClick={() => navigate(`/project-details`)}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={option.image}
+                  alt={option.alt}
+                  sx={{ objectFit: "contain", p: 1 }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {option.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {option.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
       </Stack>
     </>
   );
